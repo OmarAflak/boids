60d2d8d2425fba6846511a3a342ac2eaedfd28f9
= <=>
2022-09-02 14:38:58 -0400

    clean

--- boids/world.py
+++ boids/world.py
@@ -3,7 +3,11 @@ from boids.boid import Boid
 from boids.obstacle import Obstacle
 from boids.point import Point
 
+
 class World:
+    PERCEPTION_RADIUS = 50
+    PERCEPTION_ANGLE = math.pi / 3
+
     def __init__(self, width: int, height: int, number_of_boids: int):
         self.width = width
         self.height = height
@@ -19,8 +23,6 @@ class World:
             for i in range(0, width + 1, 150)
             for j in range(0, height + 1, 150)
         ]
-        self.perception_radius = 50
-        self.perception_angle = math.pi / 3
 
     def update(self):
         # update based on snapshot of current state
@@ -32,18 +34,18 @@ class World:
                 if b == boid:
                     continue
 
-                if b.position.distance_to(boid.position) > self.perception_radius:
+                if b.position.distance_to(boid.position) > World.PERCEPTION_RADIUS:
                     continue
 
                 neighbors.append(b)
 
             visible_obstacles = []
             for obstacle in self.obstacles:
-                if obstacle.position.distance_to(boid.position) > 2 * self.perception_radius:
+                if obstacle.position.distance_to(boid.position) > 2 * World.PERCEPTION_RADIUS:
                     continue
 
                 target = obstacle.position - boid.position
-                if abs(boid.velocity.angle_to(target)) > self.perception_angle / 2:
+                if abs(boid.velocity.angle_to(target)) > World.PERCEPTION_ANGLE / 2:
                     continue
 
                 visible_obstacles.append(obstacle)
